bbfc0daf6f6b3a6faabf52340e75bbd723350d5c
Witoslaw Koczewski <wi@koczewski.de>
2022-07-09 12:41:36 +0200

    upgrade npm packages

--- src/kunagi/mui/build.clj
+++ src/kunagi/mui/build.clj
@@ -12,11 +12,11 @@
   ;; (build/package-json-add-dependency! "react-router-dom" "^6.3.0")
   (build/package-json-add-dependency! "react-router-dom" "^5.2.0")
 
-  (build/package-json-add-dependency! "@mui/material" "^5.6.4")
-  (build/package-json-add-dependency! "@mui/styles" "^5.6.2")
-  (build/package-json-add-dependency! "@mui/icons-material" "^5.6.2")
-  (build/package-json-add-dependency! "@emotion/react" "^11.9.0")
-  (build/package-json-add-dependency! "@emotion/styled" "^11.8.1")
-  (build/package-json-add-dependency! "@fontsource/roboto" "^4.5.5")
+  (build/package-json-add-dependency! "@mui/material" "^5.8.7")
+  (build/package-json-add-dependency! "@mui/styles" "^5.8.7")
+  (build/package-json-add-dependency! "@mui/icons-material" "^5.8.4")
+  (build/package-json-add-dependency! "@emotion/react" "^11.9.3")
+  (build/package-json-add-dependency! "@emotion/styled" "^11.9.3")
+  (build/package-json-add-dependency! "@fontsource/roboto" "^4.5.7")
 
   )
