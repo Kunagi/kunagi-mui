2e3382ed4910cedd7b67c76cf11891de5a6a11c7
Witoslaw Koczewski <wi@koczewski.de>
2023-09-14 16:21:29 +0200

    browser

--- src/kunagi/mui/api.cljs
+++ src/kunagi/mui/api.cljs
@@ -6,6 +6,7 @@
    ["react-router-dom" :as router]
    [camel-snake-kebab.core :as csk]
    [clojure.string :as str]
+   [kunagi.utils.browser :as browser]
    [kunagi.mui.core :as core]
    [kunagi.utils.debug :as debug]))
 
@@ -182,47 +183,7 @@
 
 ;; url
 
-
-(defn url-params []
-  (let [params (-> js/window.location.search js/URLSearchParams.)]
-    (->> ^js params
-         .keys
-         (map str)
-         (reduce (fn [m k]
-                   (assoc m (keyword k)
-                          (.get ^js params k)))
-                 {}))))
-
-(comment
-  (url-params))
-
-(defn url-param [k]
-  (when k
-    (-> js/window.location.search
-        js/URLSearchParams.
-        (.get (cond
-                (keyword? k) (name k)
-                (string? k) k
-                :else (str k))))))
-
-(defonce URL_PARAMS (atom (url-params)))
-
-(let [push-state (.-pushState js/history)
-      replace-state (.-replaceState js/history)]
-  (set! (.-pushState js/history)
-        (fn [target state title url]
-          (let [push-state (.bind push-state js/history)]
-            (push-state target state title url))
-          (reset! URL_PARAMS (url-params))))
-  (set! (.-replaceState js/history)
-        (fn [state title url]
-          (let [replace-state (.bind replace-state js/history)]
-            (replace-state state title url)
-            (reset! URL_PARAMS (url-params)))))
-  (js/window.addEventListener "popstate" #(reset! URL_PARAMS (url-params))))
-
-
-(def use-url-params (atom-hook URL_PARAMS))
+(def use-url-params (atom-hook browser/URL_PARAMS))
 
 (defn use-url-param [k]
   (get (use-url-params)
