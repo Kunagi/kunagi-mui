6108769a9688a6534acba0d244d97750fb3a2367
Witoslaw Koczewski <wi@koczewski.de>
2024-01-31 19:52:32 +0100

    [deps]

--- src/kunagi/mui/build.clj
+++ src/kunagi/mui/build.clj
@@ -2,20 +2,21 @@
   (:require
    [kunagi.build.api :as build :refer [print-task]]))
 
+
 (defn update-package-json! []
   (print-task "package.json")
   (build/package-json-add-dependency! "react" "^17.0.2")
   (build/package-json-add-dependency! "react-dom" "^17.0.2")
-  (build/package-json-add-dependency! "react-refresh" "^0.13.0")
+  (build/package-json-add-dependency! "react-refresh" "^0.14.0")
 
-  ;; (build/package-json-add-dependency! "react-router-dom" "^6.3.0")
+  ;; (build/package-json-add-dependency! "react-router-dom" "^6.21.3")
   (build/package-json-add-dependency! "react-router-dom" "^5.2.0")
 
-  (build/package-json-add-dependency! "@mui/material" "^5.11.14")
-  (build/package-json-add-dependency! "@mui/styles" "^5.11.13")
-  (build/package-json-add-dependency! "@mui/icons-material" "^5.11.11")
-  (build/package-json-add-dependency! "@emotion/react" "^11.10.6")
-  (build/package-json-add-dependency! "@emotion/styled" "^11.10.6")
-  (build/package-json-add-dependency! "@fontsource/roboto" "^4.5.8")
+  (build/package-json-add-dependency! "@mui/material" "^5.15.7")
+  (build/package-json-add-dependency! "@mui/styles" "^5.15.7")
+  (build/package-json-add-dependency! "@mui/icons-material" "^5.15.7")
+  (build/package-json-add-dependency! "@emotion/react" "^11.11.3")
+  (build/package-json-add-dependency! "@emotion/styled" "^11.11.0")
+  (build/package-json-add-dependency! "@fontsource/roboto" "^5.0.8")
 
   )
